f1147aa6f56963e1b9f49880b07c19ac63bdfa16
Vale BM <valeryabml@gmail.com>
2024-02-29 08:18:06 -0600

    login

--- lib/screens/encuesta_screen.dart
+++ lib/screens/encuesta_screen.dart
@@ -1,4 +1,22 @@
+import 'package:app_encuesta/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+List<String> secciones = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '13'
+];
 
 class EncuestaScreen extends StatefulWidget {
   const EncuestaScreen({super.key});
@@ -10,6 +28,41 @@ class EncuestaScreen extends StatefulWidget {
 class _EncuestaScreenState extends State<EncuestaScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Column(
+      children: [
+        Container(
+          height: 250,
+          decoration: const BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30),
+            ),
+          ),
+        ),
+        Text('Secciones'),
+        SizedBox(
+          height: 500,
+          child: ListView.separated(
+              itemBuilder: (BuildContext context, int index) {
+                return GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  children: List.generate(13, (index) {
+                    print('object');
+                    return Text('a: $index');
+                  }),
+                );
+              },
+              separatorBuilder: (context, int) {
+                return const Divider(
+                  color: Colors.black,
+                );
+              },
+              itemCount: 2),
+        ),
+      ],
+    ));
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:app_encuesta/background.dart';
+import 'package:app_encuesta/screens/encuesta_screen.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -11,16 +12,31 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Stack(
         children: [
-          Background(),
-          BackgroundDown(),
-          Column(
-            children: [
-              Text('suaurio'),
-              Text('password'),
-            ],
+          const Background(),
+          const BackgroundDown(),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('suaurio'),
+                Text('password'),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return EncuestaScreen();
+                          },
+                        ),
+                      );
+                    },
+                    child: Text('Iniciar Sesión'))
+              ],
+            ),
           ),
         ],
       ),
